5098ac437cd2890f597fe28d1586a293408d9b3d
Wikkie <vigneshrgm962950@gmail.com>
2023-08-24 23:10:05 +0530

    Experiment..

--- Server/src/main/java/com/wikkie/quickz/Config/JwtService.java
+++ Server/src/main/java/com/wikkie/quickz/Config/JwtService.java
@@ -1,9 +1,18 @@
 package com.wikkie.quickz.Config;
 
+import java.security.Key;
+
+import java.util.function.Function;
 import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtService {
+    private String SECRET = "efce80366a8be898b4155661ec9d43700736148916490332d69d451141a7d4cd";
 
     public String extractUsername(String token) {
 
@@ -11,4 +20,24 @@ public class JwtService {
 
     }
 
+    public <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSigningKeys())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSigningKeys() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 }
